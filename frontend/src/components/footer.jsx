6857c436d8d4cf6962e2cc86f7a38b9bf13423c7
chris <chrisdennis117@gmail.com>
2024-07-09 17:23:33 -0400

    Initial commit

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <footer className="footer">
                 <div className="footer-info">
                     <p>Tempest Type 1.0</p>
-                    <a href="" target="_blank">GitHub</a>
+                    <a href="https://github.com/chris-dennis/tempest-type" target="_blank">GitHub</a>
                     <a href="https://cdennis.dev" target="_blank">cdennis.dev</a>
                 </div>
         </footer>
