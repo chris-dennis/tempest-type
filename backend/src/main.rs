b6f7f56ccc2ca4414ffabb3b43fada8dbe7bb593
chris <chrisdennis117@gmail.com>
2024-07-10 09:41:27 -0400

    tls

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -287,9 +287,11 @@ async fn main() -> std::io::Result<()> {
     println!("starting HTTP server");
     let party_manager = PartyManager.start();
 
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder.set_private_key_file("/etc/letsencrypt/privkey.pem", SslFiletype::PEM).unwrap();
-    builder.set_certificate_chain_file("/etc/letsencrypt/fullchain.pem").unwrap();
+    // let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    // builder.set_private_key_file("/etc/letsencrypt/privkey.pem", SslFiletype::PEM)
+    //     .expect("Failed to set private key file");
+    // builder.set_certificate_chain_file("/etc/letsencrypt/fullchain.pem")
+    //     .expect("Failed to set certificate chain file");
 
 
     HttpServer::new(move || {
@@ -305,7 +307,8 @@ async fn main() -> std::io::Result<()> {
             .service(get_or_create_user)
 
     })
-        .bind_openssl("0.0.0.0:8080", builder)?
+        .bind(("0.0.0.0", 8080))?
+        // .bind_openssl("0.0.0.0:8080", builder)?
         .run()
         .await
 }
